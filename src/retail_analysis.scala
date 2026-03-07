2469372e02009df2f809b83bab3231ba78b32f42
ebanks28 <banksevan45@gmail.com>
2026-03-06 22:07:32 -0500

    Add files via upload
    Added Spark / Spark SQL code to perform query of total revenue per product.

---
+++ src/retail_analysis.scala
@@ -0,0 +1,16 @@
+import org.apache.spark.sql.functions._
+
+val retail = spark.read.option("header", "true").option("inferSchema", "true").csv("hdfs://localhost:9000/retail/retail.csv")
+retail.printSchema()
+retail.count()
+
+val cleanRetail = retail.filter(!col("InvoiceNo").startsWith("C"))
+cleanRetail.count()
+
+val scaled10 = cleanRetail.crossJoin(spark.range(10).toDF("replication_id"))
+scaled10.count()
+
+val result = scaled10.withColumn("Revenue", col("Quantity") * col("UnitPrice")).groupBy("StockCode").agg(sum("Revenue").alias("TotalRevenue")).orderBy(desc("TotalRevenue"))
+result.show()
+
+spark.stop()
